83950033d8b39273a6ec88e6318fabf6ff93a486
Adam Vasyliuta <adamvasyliuta@gmail.com>
2022-11-27 17:26:39 +0200

    Added solution for task 2.2

---
+++ Prometheus_2.2/Prometheus_2_2.py
@@ -0,0 +1,6 @@
+station_from = "Київ" 
+station_to= "Сімферополь"
+
+print("Поїзд їде зі станції "+ station_from)
+print("Поїзд їде до станції " + station_to)
+print("Поїзд їде зі станції " + station_from + " до станції " + station_to)
