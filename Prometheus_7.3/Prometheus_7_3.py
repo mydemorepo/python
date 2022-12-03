c77ec4b37a3320aec8686476c3d28c6b2262a2eb
Adam Vasyliuta <adamvasyliuta@gmail.com>
2022-12-03 14:29:39 +0200

    Added solution for task 7.3

---
+++ Prometheus_7.3/Prometheus_7_3.py
@@ -0,0 +1,10 @@
+number = int(input("Введіть число"))
+
+
+def print_numbers(number):
+    for i in range(0, number + 1):
+        if (i % 2 == 0):
+            print(i)
+
+
+print_numbers(number)            
