cf83cfcbc16725e71065feab64a7a5bce96ee428
Adam Vasyliuta <adamvasyliuta@gmail.com>
2022-12-03 15:40:34 +0200

    Added solution for Exercise 10

---
+++ 1Pythonguide tasks/Section_3/Exercises/Exercise_10.py
@@ -0,0 +1,13 @@
+poem = '''Yes, I'll smile, indeed, through tears and weeping
+Sing my songs where evil holds its sway,
+Hopeless, a steadfast hope forever keeping,
+I shall live! You thoughts of grief, away!'''
+
+print(poem[0:15])
+print(len(poem))
+print(poem.startswith("Yes"))
+print(poem.endswith("I shall live!"))
+print(poem.find(","))
+print(poem.rfind(","))
+print(poem.count(','))
+print(poem.isalnum())
