44c0364a16e170d0d60e253dfd59d96065ba8b60
Adam Vasyliuta <adamvasyliuta@gmail.com>
2022-12-04 11:11:23 +0200

    The solution of task 10 has been fixed

--- 1Pythonguide tasks/Section_3/Tasks/task_ten.py
+++ 1Pythonguide tasks/Section_3/Tasks/task_ten.py
@@ -1,13 +1,12 @@
 import math
 
-x1 = float(input("Введіть широту першої точки: "))
-y1 = float(input("Введіть довготу першої точки: "))
-x2 = float(input("Введіть широту другої точки: "))
-y2 = float(input("Введіть довготу другої точки: "))
+x1 = math.radians(float(input("Введіть широту першої точки: ")))
+y1 = math.radians(float(input("Введіть довготу першої точки: ")))
+x2 = math.radians(float(input("Введіть широту другої точки: ")))
+y2 = math.radians(float(input("Введіть довготу другої точки: ")))
 
-print('{0:>10.3f}'.format(6371.032 *
-                  math.acos(math.sin(math.radians(x1)) *
-                            math.sin(math.radians(x2)) *
-                            math.cos(math.radians(x1)) *
-                            math.cos(math.radians(x2)) *
-                            math.cos(math.radians(y1) - math.radians(y2)))))
+print('{0:>10.3f}'.format(6371.032 * math.acos(math.sin(x1) *
+                          math.sin(x2) +
+                          math.cos(x1) *
+                          math.cos(x2) *
+                          math.cos(y1 - y2))))
