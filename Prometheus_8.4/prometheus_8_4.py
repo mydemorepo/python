316a67f267753c60854f045dfd9532733a860d32
Adam Vasyliuta <adamvasyliuta@gmail.com>
2022-12-10 16:44:00 +0200

    Added solution for task 8.4

---
+++ Prometheus_8.4/prometheus_8_4.py
@@ -0,0 +1,10 @@
+value1 = input("Введіть перший елемент ")
+value2 = input("Введіть другий елемент ")
+value3 = input("Введіть третій елемент ")
+value4 = input("Введіть четвертий елемент ")
+value5 = input("Введіть п'ятий елемент ")
+
+numbers1 = (value1, value2, value3, value4, value5)
+numbers2 = numbers1[len(numbers1)-2:]
+
+print(numbers2)
