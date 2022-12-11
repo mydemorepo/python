9ec7259f25ccc5373918a51f773b2c9db57588aa
Adam Vasyliuta <adamvasyliuta@gmail.com>
2022-12-11 17:11:56 +0200

    Added solution for task 8.4

---
+++ Prometheus_8.10/Prometheus_8_10.py
@@ -0,0 +1,9 @@
+last_name = input("Введіть прізвище ")
+name = input("Введіть ім'я ")
+second_name = input("Введіть по батькові ")
+
+if (len(last_name) < 1 or len(name) < 1 or len(second_name) < 1):
+    print("Не введений ключовий параметр")
+else:
+    print('{0} {1}{2}'.format(last_name, name.replace(name[1:], '.'),
+                              second_name.replace(second_name[1:], '.',)))
