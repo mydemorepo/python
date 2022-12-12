12bd4e742ab19b5a4708e7ffdb620a15067e2e55
Adam Vasyliuta <adamvasyliuta@gmail.com>
2022-12-12 20:47:04 +0200

    Added solution for task 3

---
+++ 1Pythonguide tasks/Section_4/Tasks/task_three.py
@@ -0,0 +1,5 @@
+cities = ['Budapest', 'Rome', 'Istanbul', 'Sydney', 'Kyiv', 'Hong Kong']
+
+print('{}, {}, {}, {}, {} and {}'.format(cities[0],
+      cities[1],
+      cities[2], cities[3], cities[4], cities[5]))
